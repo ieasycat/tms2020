20701f95f0f971a08c9d5aacb7ef3bfc88b022d9
Anton klimenka <ieasycat@hotmail.com>
2020-12-04 15:56:35 +0300

    HW04_05

---
+++ basics/basics_05.py
@@ -0,0 +1,6 @@
+text_1 = input("Введите одно любое слово: ")
+text_2 = input("Введите еще одно, любое слово: ")
+text_v1 = f'{text_1} {text_2}'
+print(text_v1)
+text_v2 = f'!{text_2} {text_1}!'
+print(text_v2)
