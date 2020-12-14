d6a399a08a55e74748bb7212d670b31c0d6c3782
Anton klimenka <ieasycat@hotmail.com>
2020-12-14 21:03:57 +0300

    HW06_12

--- struct_prog/struct_prog_12.py
+++ struct_prog/struct_prog_12.py
@@ -3,11 +3,11 @@
 Подсчитать сколько четных чисел в списке
 """
 
-listt = [2, 4, 5, 8, 13, 18, 42, 99]
-listt_new = []
+listt = [2, 4, 5, 8, 13, 18, 42, 99, 66]
 i = 0
+count = 0
 while i < len(listt):
     if listt[i] % 2 == 0:
-        listt_new.append(listt[i])
+        count += 1
     i += 1
-print(len(listt_new))
+print(count)
