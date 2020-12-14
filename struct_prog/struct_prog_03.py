a9e952dc20494eee9e6da2b974dcf17c8c64e198
Anton klimenka <ieasycat@hotmail.com>
2020-12-14 20:54:33 +0300

    HW06_03

--- struct_prog/struct_prog_03.py
+++ struct_prog/struct_prog_03.py
@@ -7,10 +7,16 @@ money_r = int(input("Введите количество рублей: "))
 money_k = int(input("Введите количество копеек: "))
 x = money_r % 100 < 10 or money_r % 100 > 20
 if money_r % 10 == 1 and money_r != 11:
-    if money_k % 10 == 1 and money_k != 11:
-        print(f"{money_r} рубль {money_k} копейка")
+    a = "рубль"
 elif money_r % 10 >= 2 and money_r % 10 <= 4 and x:
-    if money_k % 10 >= 2 and money_k % 10 <= 4 and x:
-        print(f"{money_r} рубля {money_k} копейки")
+    a = "рубля"
 else:
-    print(f"{money_r} рублей {money_k} копеек")
+    a = "рублей"
+
+if money_k % 10 == 1 and money_k != 11:
+    b = "копейка"
+elif money_k % 10 >= 2 and money_k % 10 <= 4 and x:
+    b = "копейки"
+else:
+    b = "копеек"
+print(f'{money_r} {a} {money_k} {b}')
