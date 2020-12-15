c5b34c2c7287761dc27c8c17b331de89d7cb151b
Anton Klimenka <ieasycat@hotmail.com>
2020-12-15 21:45:49 +0300

    loops_11

--- loops/loops_11.py
+++ loops/loops_11.py
@@ -13,8 +13,8 @@ for i in range(n):
     for j in range(m):
         row.append(randint(1, 9))
     list_one.append(row)
-for row in list_one:
-    for elemnt in row:
+for row in list_one:        # для каждой строки в списке
+    for elemnt in row:      # для каждого элемента в строке
         if elemnt == 7:
             count += 1
 print(f'{count} кол-во 7-ок в матрице')
