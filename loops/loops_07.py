7786c3c2b6b9a99592d149797961954dad975c84
Anton Klimenka <ieasycat@hotmail.com>
2020-12-15 20:32:08 +0300

    loops_07

--- loops/loops_07.py
+++ loops/loops_07.py
@@ -5,7 +5,7 @@
 n = int(input("Введите 1-ое число: "))
 m = int(input("Введите 2-ое число: "))
 summ = 0
-not n < m and not m > n
-for i in range(n, m + 1):
-    summ += i ** 3
+if not n < m and not m > n:
+    for i in range(n, m + 1):
+        summ += i ** 3
 print(summ)
