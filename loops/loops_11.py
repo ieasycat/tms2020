78ea715e7e900f1b22cd03238384a9405780ec6a
Anton Klimenka <ieasycat@hotmail.com>
2020-12-15 21:44:55 +0300

    loops_11

---
+++ loops/loops_11.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+
+from random import randint
+
+n = int(input("Введите количество раз: "))
+m = int(input("Введите количество раз: "))
+list_one = []
+count = 0
+for i in range(n):
+    row = []
+    for j in range(m):
+        row.append(randint(1, 9))
+    list_one.append(row)
+for row in list_one:
+    for elemnt in row:
+        if elemnt == 7:
+            count += 1
+print(f'{count} кол-во 7-ок в матрице')
+
+for row in list_one:
+    for elemnt in row:
+        print(elemnt, end=' ')
+    print()
