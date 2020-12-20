6b4beefb7d28c4155d1618342cdc81a05bc50fe7
Anton klimenka <ieasycat@hotmail.com>
2020-12-20 23:30:20 +0300

    HW07_12

--- loops/loops_12.py
+++ loops/loops_12.py
@@ -6,27 +6,74 @@
 """
 from random import randint
 
-n = int(input("Введите количество раз: "))
-m = int(input("Введите количество раз: "))
-list_one = []
-list_two = []
-count = 0
-numbers = 0
-for i in range(n):
-    row = []
-    for j in range(m):
-        row.append(randint(1, 9))
-    list_one.append(row)
-    list_two += list_one[i]
-a = (min(list_two) + max(list_two)) / 2
-print(list_two)
-for v in range(len(list_two)):
-    for z in range(len(list_two)):
-        if v + z % 2 == 0:
-            numbers += list_two[v]
-    print(numbers)
-# print(list_two)
-"""for x in list_two:
-    if x > a:
-        count += 1
-print(count)"""
+
+def create_matrix(n, m):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(randint(1, 9))
+        matrix.append(row)
+    return matrix
+
+
+def min_matrix(matrix):
+    m = matrix[0][0]
+    for row in matrix:
+        for elem in row:
+            if elem < m:
+                m = elem
+    return m
+
+
+def max_matrix(matrix):
+    m = matrix[0][0]
+    for row in matrix:
+        for elem in row:
+            if elem > m:
+                m = elem
+    return m
+
+
+def my_average(a, b):
+    average = (a + b) / 2
+    return average
+
+
+def my_summ(matrix):
+    tmp = []
+    for v in range(len(matrix)):
+        for z in range(len(matrix)):
+            if (v + z) % 2 == 0:
+                tmp += [matrix[v][z]]
+    return tmp
+
+
+def result(summ, avarage):
+    count = 0
+    for elem in summ:
+        if elem > avarage:
+            count += 1
+    return count
+
+
+def main():
+    n = int(input("Введите количество раз: "))
+    m = int(input("Введите количество раз: "))
+    print_matrix = create_matrix(n, m)
+    summ = my_summ(print_matrix)
+    max_number = max_matrix(print_matrix)
+    min_number = min_matrix(print_matrix)
+    average_number = my_average(min_number, max_number)
+    print_result = result(summ, average_number)
+    print(f'Вот матрица: {print_matrix}')
+    print(f'Вот среднее арифметическое значение: {average_number}')
+    print(f'Вот элемменты, где сумма индексов четна: {summ}')
+    print(f'Вот количество элементов матрицы, '
+          f'превосходящих среднее арифметическое '
+          f'значение элементов матрицы и '
+          f'сумма индексов которых четна - {print_result} ')
+
+
+if __name__ == '__main__':
+    main()
