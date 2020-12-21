b0aba9212aec8800b144a27d8d1c697f385cf54c
Anton klimenka <ieasycat@hotmail.com>
2020-12-21 23:01:17 +0300

    HW08_19

--- struct_prog/struct_prog_19.py
+++ struct_prog/struct_prog_19.py
@@ -7,8 +7,7 @@ S=1+1/2+1/3+1/4+...+1/N, где N – натуральное число. [02-3.2
 def my_summ(x):
     summ = 0
     for i in range(1, x + 1):
-        if x != 0:
-            summ += 1/i
+        summ += 1/i
     return summ
 
 
