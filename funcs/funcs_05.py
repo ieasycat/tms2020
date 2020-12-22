6486eb66d042543f2203d59ef295db19249b3af6
Anton Klimenka <ieasycat@hotmail.com>
2020-12-22 20:33:11 +0300

    funcs_05

--- funcs/funcs_05.py
+++ funcs/funcs_05.py
@@ -8,8 +8,8 @@
 
 def my_summ(*args):
     summ = 0
-    for i in range(len(args)):
-        summ += args[i] * i
+    for elem in range(len(args)):
+        summ += args[elem] * elem
     return summ
 
 
