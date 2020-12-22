2ba28ee9686ee28ae89fea1ab09ff77d3a3f92cc
Anton klimenka <ieasycat@hotmail.com>
2020-12-22 13:53:35 +0300

    HW08_21

--- struct_prog/struct_prog_21.py
+++ struct_prog/struct_prog_21.py
@@ -10,17 +10,22 @@ def my_print(x):
     result = 0
     for i in range(len(x)-2):
         if x[i + 2] > x[i + 1] > x[i]:
-            count += 1
-        elif count >= 1 and x[i + 1] > x[i + 2]:
+            if x[i + 2] > x[i + 1]:
+                count += 1
+        elif count >= 1 and x[i + 1] > x[i + 2] or x[i + 1] > x[i]:
             result += 1
             count = 0
-    if x[-1] > x[-2] > x[-3]:
+    if count >= 1:
+        result += 1
+        count = 0
+    if x[-1] > x[-2] <= x[-3]:
         result += 1
     return result
 
 
 def main():
-    array = [27, 17, 18, 19, 20, 1, 24, 25]
+    array = [1, 2, 27, 16, 17, 17, 18, 19, 20, 1,
+             24, 25, 25, 26, 31, 30, 36, 37]
     print_array = my_print(array)
     print(f'Массиив: {array}')
     print(f'Количество возрастанний: {print_array}')
