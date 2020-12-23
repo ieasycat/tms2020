372798ab35b7abcf2a6bcaada9784f96f5d17c29
Anton klimenka <ieasycat@hotmail.com>
2020-12-23 15:06:00 +0300

    funcs_08

--- funcs/funcs_08.py
+++ funcs/funcs_08.py
@@ -6,27 +6,33 @@
 """
 
 
-def mean_type():
-    pass
-
-
-"""def arithmetic(*args):
+def arithmetic(*args):
     arithmetic_elem = 0
+    summ = 0
     for elem in args:
-        summ = 0
         summ += elem
     arithmetic_elem = summ / len(args)
-    return arithmetic_elem"""
+    print(f'Среднеарифметическое значение равно - {arithmetic_elem}')
 
 
-def geometric():
-    pass
+def geometric(*args):
+    geometric_elem = 0
+    summ = 1
+    for elem in args:
+        summ *= elem
+    geometric_elem = summ ** (1/len(args))
+    print(f'Среднегеометрическое значение равно - {geometric_elem}')
+
+
+def choice(mean_type='geometric', *args):
+    if mean_type == 'arithmetic':
+        arithmetic(*args)
+    elif mean_type == 'geometric':
+        geometric(*args)
 
 
 def main():
-    mean_type()
-    # a = arithmetic()
-    # g = geometric()
+    choice('arithmetic', 2, 4, 6, 9, 10, 11, 7, 12)
 
 
 if __name__ == '__main__':
