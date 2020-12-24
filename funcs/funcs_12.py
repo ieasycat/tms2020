01a8313e0713c7fd5cca51e6946dccd95a838b2a
Anton klimenka <ieasycat@hotmail.com>
2020-12-25 00:00:30 +0300

    funcs_12

--- funcs/funcs_12.py
+++ funcs/funcs_12.py
@@ -22,7 +22,13 @@ def main():
     one_number = int(input("Введите 1-ое число: "))
     two_number = int(input("Введите 2-ое число: "))
     pr_power = is_power_n(one_number, two_number)
-    print(pr_power)
+    print(f'Это первое решение - {pr_power}')
+    test = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    count = 0
+    for my_number in range(len(test)):
+        if is_power_n(64, test[my_number]) is True:
+            count += 1
+    print(f'А это вторая чать задачки - {count}')
 
 
 if __name__ == '__main__':
