90f8c560692fc57a85b0237b649ce536f7410369
Anton klimenka <ieasycat@hotmail.com>
2020-12-28 23:25:53 +0300

    HW10_25

--- struct_prog/struct_prog_25.py
+++ struct_prog/struct_prog_25.py
@@ -16,13 +16,13 @@ def calculation_trains(trains):
     for train in trains:
         a = train['arrival time']
         b = train['departure time']
-        if a - b > timedelta(hours=7, minutes=20):
+        if a - b > timedelta(days=0, hours=7, minutes=20):
             print(f"Номер поезда: {train['number']}, "
                   f"следует: {train['p_departure']} - {train['p_arrival']}, "
                   f"время отправления: {b}, "
                   f"время прибытия {a}")
             print(f"Время в пути - {a - b}")
-        print()
+            print()
 
 
 def main():
@@ -30,23 +30,37 @@ def main():
         {
             'number': '140-139',
             'p_arrival': 'Москва',
-            'arrival time': datetime(2020, 12, 26, hour=19, minute=50),
+            'arrival time': datetime(2020, 12, 26, 19, 50),
             'p_departure': 'Минск',
-            'departure time': datetime(2020, 12, 26, hour=10, minute=5),
+            'departure time': datetime(2020, 12, 26, 10, 5),
         },
         {
             'number': '140-252',
             'p_arrival': 'Гродно',
-            'arrival time': datetime(2020, 12, 26, hour=14, minute=50),
+            'arrival time': datetime(2020, 12, 26, 14, 50),
             'p_departure': 'Минск',
-            'departure time': datetime(2020, 12, 26, hour=9, minute=35),
+            'departure time': datetime(2020, 12, 26, 9, 35),
         },
         {
             'number': '10-115',
             'p_arrival': 'Орша',
-            'arrival time': datetime(2020, 12, 26, hour=20, minute=5),
+            'arrival time': datetime(2020, 12, 26, 20, 5),
             'p_departure': 'Брест',
-            'departure time': datetime(2020, 12, 26, hour=7, minute=35),
+            'departure time': datetime(2020, 12, 26, 7, 35),
+        },
+        {
+            'number': '001Э',
+            'p_arrival': 'Владивосток',
+            'arrival time': datetime(2021, 1, 19, 20, 5),
+            'p_departure': 'Москва',
+            'departure time': datetime(2021, 1, 12, 17, 35),
+        },
+        {
+            'number': '082И',
+            'p_arrival': 'Иркутск',
+            'arrival time': datetime(2021, 1, 4, 00, 45),
+            'p_departure': 'Москва',
+            'departure time': datetime(2020, 12, 31, 13, 10),
         },
     ]
     calculation_trains(trains_listt)
