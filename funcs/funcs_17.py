e41450600c53f4abd41935a66d179c87ddb554c1
Anton klimenka <ieasycat@hotmail.com>
2020-12-29 15:36:54 +0300

    HW10_17

--- funcs/funcs_17.py
+++ funcs/funcs_17.py
@@ -8,14 +8,31 @@ sin( x ) = x – x ^3 /(3!) + x^ 5 /(5!) – ...
 С помощью Sin1 найти приближенное значение синуса
 для данного x при шести данных ε .  [01-11.3-Proc41]
 """
+import math
 
 
 def sin1(x, eps):
-    pass
+    n = 0
+    value = x
+    summ = 0
+    while abs(value) > eps:
+        f = 2 * n + 1
+        new_f = 1
+        for s in range(1, f + 1):
+            new_f *= s
+        value = (-1) ** n * x ** (2 * n + 1) / new_f
+        n += 1
+        summ += value
+    return summ
 
 
 def main():
-    pass
+    eps = 0.01
+    number = math.pi/6
+    for i in range(0, 6):
+        f = sin1(number, eps)
+        eps /= 10
+        print(f'eps= {eps}, sin({number})= {f}')
 
 
 if __name__ == '__main__':
