1a52b85e7b1a599d6d5619e210d48c2f5a8311e2
Anton klimenka <ieasycat@hotmail.com>
2021-01-05 11:47:06 +0300

    HW11_03

---
+++ files/files_03.py
@@ -0,0 +1,23 @@
+"""
+В конец существующего текстового файла записать три новые строки текста.
+Записываемые строки вводятся с клавиатуры. [03-15.6]
+"""
+
+
+def write_file():
+    with open('files_03', 'a') as my_file:
+        for i in range(3):
+            line = input('Введите строку: ')
+            my_file.write(line + '\n')
+    return my_file
+
+
+def main():
+    write_file()
+    with open('files_03') as file:
+        pr_line = file.readlines()
+        print(pr_line)
+
+
+if __name__ == '__main__':
+    main()
