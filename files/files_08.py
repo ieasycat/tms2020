dadb9f0b796977b5d063f836ad6256612f261089
Anton klimenka <ieasycat@hotmail.com>
2021-01-13 00:09:53 +0300

    files_08

---
+++ files/files_08.py
@@ -0,0 +1,41 @@
+"""
+В файле files_08 импортировать функции.
+С помощью функций создать файл с информацией о товарах
+(Имя товара, цена, количество, комментарий).
+Прочесть файл, Добавить новую позицию в конец. Удалить третью строку.
+"""
+
+from files import csv_utils
+
+
+def create_file(file):
+    title = ['Имя товара', 'цена', 'количество', 'комментарий']
+    info_product = [
+        ['Ручка', '1 рубль 5 копеек', '50', 'гелевая синего цвета'],
+        ['Тетрадь в клетку', '3 рубля', '200', 'в мелкую клетку'],
+        ['Ластик', '50 копеек', '700', 'твердый']
+    ]
+    csv_utils.write(file, title, info_product)
+
+
+def new_poz(text):
+    new_product = ['Замазка', '80 копеек', '500', '500мл']
+    csv_utils.add(text, new_product)
+
+
+def dell_poz(text):
+    csv_utils.dell(text, 3)
+
+
+def main():
+    file = 'files_08.txt'
+    create_file(file)
+    name, info = csv_utils.read(file)
+    print(f'Заголовок - {name}')
+    print(f'Информация - {info}')
+    new_poz(file)
+    dell_poz(file)
+
+
+if __name__ == '__main__':
+    main()
