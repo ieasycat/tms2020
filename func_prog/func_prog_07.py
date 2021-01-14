cf1149dfb2dadfd1530cd54f3e9e7d47a204e5f6
Anton Klimenka <ieasycat@hotmail.com>
2021-01-14 21:58:24 +0300

    func_prog_07

--- func_prog/func_prog_07.py
+++ func_prog/func_prog_07.py
@@ -6,7 +6,7 @@ from functools import reduce
 
 def main():
     print(reduce(lambda a, x: a * x,
-                 filter(lambda x: x % 3 == 0, [2, 3, 5, 6])))
+                 filter(lambda x: x % 3 == 0, [2, 3, 5, 6]), 1))
 
 
 if __name__ == '__main__':
