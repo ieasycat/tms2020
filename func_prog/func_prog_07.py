6da709bef1e90b58c531337946d6fbc63f5dd071
Anton Klimenka <ieasycat@hotmail.com>
2021-01-14 21:56:13 +0300

    func_prog_07

---
+++ func_prog/func_prog_07.py
@@ -0,0 +1,13 @@
+"""
+Дан список чисел. Найти произведение всех чисел, которые кратны 3.
+"""
+from functools import reduce
+
+
+def main():
+    print(reduce(lambda a, x: a * x,
+                 filter(lambda x: x % 3 == 0, [2, 3, 5, 6])))
+
+
+if __name__ == '__main__':
+    main()
