c46c27587c6eafd006eaaa1d8401d1158e2c8cee
Anton klimenka <ieasycat@hotmail.com>
2021-01-16 15:15:29 +0300

    HW13_12

--- files/files_12.py
+++ files/files_12.py
@@ -3,37 +3,24 @@
 Каждая дата - это число, месяц и год. Найти самую раннюю дату. [02-8.1-ML-29]
 """
 
-from files.csv_utils import write, read
-from datetime import date
-
-
-def create_file(mt_file):
-    title = ['Дата']
-    dates = [
-        [date(2019, 9, 25)],
-        [date(2020, 12, 5)],
-        [date(1992, 8, 11)],
-        [date(2021, 1, 10)],
-        [date(2001, 10, 31)],
-        [date(1999, 5, 1)],
-        [date(2011, 4, 13)]
-    ]
-    write(mt_file, title, dates)
+from files.csv_utils import read
+from datetime import datetime
 
 
 def search(my_file):
-    title, date = read(my_file)
-    old_date = date[0]
-    for i in date:
+    title, dates = read(my_file)
+    datetime_object = []
+    for i in dates:
+        datetime_object.append(datetime.strptime(i[0], '%d.%m.%Y'))
+    old_date = datetime_object[0]
+    for i in datetime_object:
         if old_date > i:
             old_date = i
-    old_date = ''.join(old_date)
-    return old_date
+    return old_date.strftime('%d.%m.%Y')
 
 
 def main():
     file = 'files_12.csv'
-    create_file(file)
     pr = search(file)
     print(pr)
 
