c010f2259124ffb21e7daa21a26468cdc58f6a48
Anton klimenka <ieasycat@hotmail.com>
2021-01-16 14:12:59 +0300

    HW13_09

--- func_prog/func_prog_09.py
+++ func_prog/func_prog_09.py
@@ -3,3 +3,13 @@
 Отформатировать все строки в формате ‘{i} - {string}’,
 где i это порядковый номер строки в списке. Использовать генератор списков.
 """
+
+
+def main():
+    listt = [f'{i} - {k}'
+             for i, k in enumerate(['Alo', 'Hello', 'Man', 'Good'])]
+    print(listt)
+
+
+if __name__ == '__main__':
+    main()
