9104a6b0ab1bed95fc8f2a9feb91213031f0f155
Anton Klimenka <ieasycat@hotmail.com>
2021-01-19 19:28:51 +0300

    csv_utilis

--- files/csv_utils.py
+++ files/csv_utils.py
@@ -44,11 +44,55 @@ def dell(text, place=-1):
     write(text, fields, data)
 
 
+def my_summ(info):
+    summ = 0
+    for i, number in enumerate(info):
+        tmp = int(info[i][1]) * int(info[i][2])
+        summ += tmp
+    return summ
+
+
+def expensive(info):
+    max = 0
+    for i, number in enumerate(info):
+        tmp = int(info[i][1])
+        if max < tmp:
+            max = tmp
+    max = str(max)
+    for i, k in enumerate(info):
+        if max in info[i][1]:
+            return info[i][0]
+
+
+def cheap(info):
+    my_min = int(info[0][1])
+    for i, number in enumerate(info):
+        tmp = int(info[i][1])
+        if my_min > tmp:
+            my_min = tmp
+    my_min = str(my_min)
+    for i, k in enumerate(info):
+        if my_min in info[i][1]:
+            return info[i][0]
+
+
+def decrease(file, product, n=1):
+    title, info_product = read(file)
+    for i, numbers in enumerate(info_product):
+        if i == product:
+            info_product[i][2] = int(info_product[i][2]) - n
+    write(file, title, info_product)
+
+
 def main():
     read()
     write()
     add()
     dell()
+    my_summ()
+    expensive()
+    cheap()
+    decrease()
 
 
 if __name__ == '__main__':
