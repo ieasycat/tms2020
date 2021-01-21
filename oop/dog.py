6210e5fe752a22264feaa0bc7b274647ea84d35d
Anton Klimenka <ieasycat@hotmail.com>
2021-01-21 21:19:29 +0300

    oop_06

--- oop/dog.py
+++ oop/dog.py
@@ -8,34 +8,41 @@ class Dog:
     def jump(self):
         print('Jump!')
 
-    def __init__(self, name, age, height, weight):
+    def __init__(self, name, age, height, weight, master):
         self.name = name
         self.age = age
         self.height = height
         self.weight = weight
+        self.__master = master
 
     def change_name(self, name):
         self.name = name
 
+    def get_master(self):
+        return self.__master
+
 
 def main():
-    dog = Dog('Bobik', 2, 2, 25)            # oop_02
-    new_dog = Dog('Bager', 1.5, 1.5, 15)
+    # oop_02
+    dog = Dog('Bobik', 2, 2, 25, "I'm")
+    new_dog = Dog('Bager', 1.5, 1.5, 15, "I'm")
     print(dog, new_dog)
-
-    dog.bark()                              # oop_03
+    # oop_03
+    dog.bark()
     dog.run()
     dog.jump()
-
-    dog_1 = Dog('Arni', 3, 1, 20)           # oop_04
+    # oop_04
+    dog_1 = Dog('Arni', 3, 1, 20, "I'm")
     print(f'Name Dog: {dog_1.name}')
     print(f'Age Dog: {dog_1.age}')
     print(f'Height Dog: {dog_1.height}')
     print(f'Weight Dog: {dog_1.weight}')
-
-    print(f'Name Dog: {dog_1.name}')        # oop_05
+    # oop_05
+    print(f'Name Dog: {dog_1.name}')
     dog_1.change_name('Big Arni')
     print(f'Name Dog: {dog_1.name}')
+    # oop_06
+    print(f'Master Dog: {dog_1.get_master()}')
 
 
 if __name__ == '__main__':
