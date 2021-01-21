6e481f15881bc0e249c1c9f78a050d819289f039
Anton Klimenka <ieasycat@hotmail.com>
2021-01-21 21:04:07 +0300

    oop_05

--- oop/dog.py
+++ oop/dog.py
@@ -1,10 +1,4 @@
 class Dog:
-    def __init__(self, name, age, height, weight):
-        self.name = name
-        self.age = age
-        self.height = height
-        self.weight = weight
-
     def bark(self):
         print('Woof Woof!')
 
@@ -14,19 +8,34 @@ class Dog:
     def jump(self):
         print('Jump!')
 
+    def __init__(self, name, age, height, weight):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+    def change_name(self, name):
+        self.name = name
+
 
 def main():
-    dog = Dog('Bobik', 2, 2, 25)
+    dog = Dog('Bobik', 2, 2, 25)            # oop_02
     new_dog = Dog('Bager', 1.5, 1.5, 15)
     print(dog, new_dog)
-    dog.bark()
+
+    dog.bark()                              # oop_03
     dog.run()
     dog.jump()
-    dog_1 = Dog('Arni', 3, 1, 20)
+
+    dog_1 = Dog('Arni', 3, 1, 20)           # oop_04
     print(f'Name Dog: {dog_1.name}')
     print(f'Age Dog: {dog_1.age}')
     print(f'Height Dog: {dog_1.height}')
     print(f'Weight Dog: {dog_1.weight}')
+
+    print(f'Name Dog: {dog_1.name}')        # oop_05
+    dog_1.change_name('Big Arni')
+    print(f'Name Dog: {dog_1.name}')
 
 
 if __name__ == '__main__':
