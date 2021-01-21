c9bd1307f1b6856e53ad09ab9ddd3e6eb920da7f
Anton Klimenka <ieasycat@hotmail.com>
2021-01-21 20:22:44 +0300

    oop_03

--- oop/dog.py
+++ oop/dog.py
@@ -1,11 +1,21 @@
 class Dog:
-    pass
+    def bark(self):
+        print('Woof Woof!')
+
+    def run(self):
+        print('Run!')
+
+    def jump(self):
+        print('Jump!')
 
 
 def main():
     dog = Dog()
     new_dog = Dog()
     print(dog, new_dog)
+    dog.bark()
+    dog.run()
+    dog.jump()
 
 
 if __name__ == '__main__':
