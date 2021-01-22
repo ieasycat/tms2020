050230e4e3e513cd98f810aa7bc876af4b5d1e2e
Anton klimenka <ieasycat@hotmail.com>
2021-01-22 13:16:19 +0300

    funcs_11

--- funcs/funcs_11.py
+++ funcs/funcs_11.py
@@ -7,25 +7,28 @@ def quadratic_equation(a, b, c):
     if b != 0 and c != 0:
         discriminant = b ** 2 - (4 * a * c)
         if discriminant < 0:
-            print('Решений нет.')
+            x1 = x2 = 0
+            return x1, x2
         elif discriminant == 0:
             x1 = -b / (2 * a)
-            print(f'Корень уравнения равны: {x1}')
+            x2 = x1
+            return x1, x2
         elif discriminant > 0:
             x1 = (-b + discriminant ** 0.5) / (2 * a)
             x2 = (-b - discriminant ** 0.5) / (2 * a)
-            print(f'Корни уравнения равны: {x1} и {x2}')
+            return x1, x2
     elif b == 0:
         if (-c / a) >= 0:
             x1 = - (-(c / a)) ** 0.5
             x2 = (-(c / a)) ** 0.5
-            print(f'Корни уравнения равны: {x1} и {x2}')
+            return x1, x2
         else:
-            print('Решений нет.')
+            x1 = x2 = 0
+            return x1, x2
     elif c == 0:
         x1 = 0
         x2 = -(b / a)
-        print(f'Корни уравнения равны: {x1} и {x2}')
+        return x1, x2
 
 
 def main():
@@ -33,7 +36,8 @@ def main():
     if one_number != 0:
         two_number = int(input("Введите 'b': "))
         tree_number = int(input("Введите 'c': "))
-        quadratic_equation(one_number, two_number, tree_number)
+        x1, x2 = quadratic_equation(one_number, two_number, tree_number)
+        print(f'Корни уравнения равны: {x1} и {x2}')
     else:
         print("'a' не может быть равно '0''.")
 
