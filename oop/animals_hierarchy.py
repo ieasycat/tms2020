7d3d3eba6186f2560643e12eac62480bb858c1ba
Anton klimenka <ieasycat@hotmail.com>
2021-01-29 12:37:14 +0300

    HW17_26

---
+++ oop/animals_hierarchy.py
@@ -0,0 +1,39 @@
+"""
+Создать файл animals_hierarchy.py. Реализовать следующую структуру.
+"""
+
+
+class Animal:
+    pass
+
+
+class Pet(Animal):
+    pass
+
+
+class Cat(Pet):
+    pass
+
+
+class Dog(Pet):
+    pass
+
+
+class WildAnimal(Animal):
+    pass
+
+
+class Lion(WildAnimal):
+    pass
+
+
+class Wolf(WildAnimal):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
