62f77a7b6b0123de85e7df7148fb004a94e15d45
Anton klimenka <ieasycat@hotmail.com>
2021-02-03 00:27:01 +0300

    scripts_03

--- scripts/scripts_03.py
+++ scripts/scripts_03.py
@@ -7,6 +7,6 @@ import sys
 
 file_path = os.path.relpath(__file__)
 dir_name = os.path.dirname(file_path)
-os.mkdir(f'{dir_name}/{sys.argv[1]}')
+os.mkdir(os.path.join(dir_name, sys.argv[1]))
 
 # Вводим(python scripts/scripts_03.py my_test_scripts)
