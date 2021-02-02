cbe941c8dde012b9c0aa0b322ca87eadaa0b6fc1
Anton Klimenka <ieasycat@hotmail.com>
2021-02-02 21:48:41 +0300

    scripts_03

---
+++ scripts/scripts_03.py
@@ -0,0 +1,10 @@
+"""
+Создать скрипт, который принимает имя папки и создает ее рядом со скриптом.
+"""
+
+import os
+import sys
+
+file_path = os.path.relpath(__file__)
+dir_name = os.path.dirname(file_path)
+os.mkdir(f'{dir_name}/{sys.argv[1]}')
