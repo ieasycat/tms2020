4646796dba17080148341b418036354d335b68b3
Anton klimenka <ieasycat@hotmail.com>
2021-02-03 00:26:00 +0300

    scripts_04

--- scripts/scripts_04.py
+++ scripts/scripts_04.py
@@ -10,7 +10,7 @@ file_path = os.path.relpath(__file__)
 dir_name = os.path.dirname(file_path)
 os.mkdir(f'{dir_name}/{sys.argv[1]}')
 
-fn = os.path.join(f'{dir_name}/{sys.argv[1]}/{sys.argv[2]}')
+fn = os.path.join(dir_name, sys.argv[1], sys.argv[2])
 with open(fn, 'w') as my_file:
     pass
 
