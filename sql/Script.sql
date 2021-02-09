c72794d4bf39666ce1dbf20c115f32e0d3b9f62f
Anton Klimenka <ieasycat@hotmail.com>
2021-02-09 21:30:19 +0300

    sql_05

--- sql/Script.sql
+++ sql/Script.sql
@@ -8,3 +8,11 @@ CREATE TABLE Book (
 
 
 ALTER TABLE Book ADD COLUMN release_year int;
+
+
+INSERT INTO Book (id, title, pages, author, price, release_year) VALUES 
+(2, 'Harry Potter', 999, 'Diana', 750.79, 2008),
+(3, 'Harry Potter 2', 807, 'Diana', 730.00, 2010),
+(4, 'Harry Potter 3', 799, 'Diana', 700.19, 2012),
+(5, 'Harry Potter 4', 809, 'Diana', 725.09, 2014),
+(6, 'Harry Potter 5', 909, 'Diana', 745.99, 2016);
