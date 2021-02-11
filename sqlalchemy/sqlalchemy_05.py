54c0c50a29d758096dbaf174d07c193635a9fcae
Anton Klimenka <ieasycat@hotmail.com>
2021-02-11 21:21:34 +0300

    sqlalchemy_05

---
+++ sqlalchemy/sqlalchemy_05.py
@@ -0,0 +1,22 @@
+"""
+Создать файл book.py.
+Создать базу book.db.
+Создать таблицу Book с помощью механизма описания таблиц sqlalchemy.
+Атрибуты: id(integer primary key), title(varchar), pages(int),
+author(varchar), price(float), release_year(int)
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, Float
+
+engine = create_engine('sqlite:///book.db', echo=True)
+metadata = MetaData()
+my_table = Table('Book', metadata,
+                 Column('id', Integer, primary_key=True),
+                 Column('title', String),
+                 Column('pages', Integer),
+                 Column('author', String),
+                 Column('price', Float),
+                 Column('release_year', Integer),
+                 )
+metadata.create_all(engine)
