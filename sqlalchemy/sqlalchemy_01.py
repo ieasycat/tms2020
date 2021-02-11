6d7f4db2ef2f37eb610589f9c5ce6640eead3bfc
Anton Klimenka <ieasycat@hotmail.com>
2021-02-11 20:16:54 +0300

    sqlalchemy_01

--- sqlalchemy/sqlalchemy_01.py
+++ sqlalchemy/sqlalchemy_01.py
@@ -1,3 +1,11 @@
+"""
+Все операции выполнять с помощью sqlalchemy.
+Создать файл sqlalchemy_01.py. Создать базу sa_test.db.
+Создать таблицу Book с помощью sqlalchemy.
+Атрибуты: id(integer primary key), title(varchar),
+pages(int), author(varchar), price(float), release_year(int)
+"""
+
 from sqlalchemy import create_engine
 
 my = create_engine('sqlite:///sa_test.db')
