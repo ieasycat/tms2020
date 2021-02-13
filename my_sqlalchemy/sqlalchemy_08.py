fe48bd33454d71e8bf7cd96c7c031fa60a8d0290
Anton klimenka <ieasycat@hotmail.com>
2021-02-13 19:18:45 +0300

    HW20_08

---
+++ my_sqlalchemy/sqlalchemy_08.py
@@ -0,0 +1,24 @@
+"""
+Создать файл sqlalchemy_08.py.
+Написать программу: пользователь вводит границы фильтрации по году.
+Отобразить на экране те книги, год которых находиться в заданных границах.
+Если таких книг нет - вывести сообщение: Not found.
+"""
+
+from my_sqlalchemy.book import Books, session
+from sqlalchemy import and_
+
+one = int(input('Введите с какого года начать фильтрацию: '))
+two = int(input('Введите по какой года закончить фильтрацию: '))
+
+books = session.query(Books).filter(and_(Books.release_year >= one,
+                                         Books.release_year <= two))
+if len(list(books)) > 0:
+    for book in books:
+        print(f'Title book: {book.title}, '
+              f'pages book: {book.pages}, '
+              f'author book: {book.author}, '
+              f'price book: {book.price}, '
+              f'release year book: {book.release_year}')
+else:
+    print('Not found!')
