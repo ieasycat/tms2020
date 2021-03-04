b0d203ead3eb98dd65f1e4afce1aab240ad5ef6f
Anton Klimenka <ieasycat@hotmail.com>
2021-03-04 21:40:27 +0300

    app1

--- src/app1/views.py
+++ src/app1/views.py
@@ -1,3 +1,13 @@
 # from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+
+
+def time(request):
+    time_now = datetime.now()
+    days = time_now.day
+    mounts = time_now.month
+    years = time_now.year
+    return HttpResponse(f'{days}-{mounts}-{years}')
