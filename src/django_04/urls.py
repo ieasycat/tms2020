f38964b88611540a38b7436aa3318933dfe890fb
Anton Klimenka <ieasycat@hotmail.com>
2021-03-09 20:39:09 +0300

    django_06

--- src/django_04/urls.py
+++ src/django_04/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from django_04.views import info
+from django_04.views import info, full_form
 
 urlpatterns = [
     path('info', info, name='info'),
+    path('full_form', full_form, name='full form')
 ]
--- src/django_04/views.py
+++ src/django_04/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -16,3 +17,20 @@ def info(request):
                                  f'Last name: {last_name}\n'
                                  f'Old: {old}\n\n'])
     return HttpResponse(response)
+
+
+def full_form(request):
+    template = loader.get_template('django_06_form.html')
+    response = template.render({}, request)
+    if request.method == 'GET':
+        return HttpResponse(response)
+    else:
+        first_name = request.POST.get('firstname')
+        last_name = request.POST.get('lastname')
+        old = request.POST.get('old')
+        context = {
+            'first name': first_name,
+            'last name': last_name,
+            'old': old,
+        }
+        return render(request, 'django_06_display.html', context)
