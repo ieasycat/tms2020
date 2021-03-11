d1d001eb450c33e6bf1498687e6b4ec978a03793
Anton Klimenka <ieasycat@hotmail.com>
2021-03-11 21:22:37 +0300

    django_16

--- src/school/admin.py
+++ src/school/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from school.models import Group, Student
+from school.models import Group, Student, Diary
 
 # Register your models here.
 
 admin.site.register(Group)
 admin.site.register(Student)
+admin.site.register(Diary)
--- src/school/models.py
+++ src/school/models.py
@@ -10,6 +10,19 @@ class Group(models.Model):
         return f'{self.pk} - {self.name}'
 
 
+class Diary(models.Model):
+    average_score = models.FloatField()
+    student = models.OneToOneField(
+        'Student',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='diary'
+    )
+
+    def __str__(self):
+        return f'{self.pk} - {self.student.firstname} - {self.student.lastname} - {self.student.group.name} - {self.average_score}'
+
+
 class Student(models.Model):
     firstname = models.CharField(max_length=25)
     lastname = models.CharField(max_length=25)
