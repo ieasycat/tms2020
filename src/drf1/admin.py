9e50849f2767e9a0aaba6a39350aeceb4befb0c9
Anton Klimenka <ieasycat@hotmail.com>
2021-03-16 21:15:39 +0300

    drf_01

--- src/drf1/admin.py
+++ src/drf1/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from drf1.models import Product
+from django.contrib.admin import ModelAdmin
 
 # Register your models here.
 
-admin.site.register(Product)
+
+class ProductAdmin(ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'price',
+        'counter',
+        'comment',
+    ]
+
+
+admin.site.register(Product, ProductAdmin)
