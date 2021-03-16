0658f4cfd63abc723f552dcbffcead4d2e63b469
Anton Klimenka <ieasycat@hotmail.com>
2021-03-16 21:45:19 +0300

    drf_03

--- src/drf1/views.py
+++ src/drf1/views.py
@@ -1,12 +1,14 @@
 from drf1.models import Product
 from drf1.serializers import ProductSerializers
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def api_products(request):
     if request.method == 'GET':
         products = Product.objects.all()
         serializers = ProductSerializers(products, many=True)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
