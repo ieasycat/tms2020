9776a96abb891f17200e8b94c7a7f4816dd3aeb4
Anton klimenka <ieasycat@hotmail.com>
2021-03-17 14:48:00 +0300

    drf_05

--- src/drf1/views.py
+++ src/drf1/views.py
@@ -2,16 +2,23 @@ from drf1.models import Product
 from drf1.serializers import ProductSerializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def api_products(request):
     if request.method == 'GET':
         products = Product.objects.all()
         serializers = ProductSerializers(products, many=True)
         return Response(serializers.data)
+    elif request.method == 'POST':
+        serializers = ProductSerializers(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
