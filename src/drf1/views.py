f739d6be24db883ffb7e97a198cbc95892ca6d58
Anton Klimenka <ieasycat@hotmail.com>
2021-03-18 20:11:45 +0300

    drf_08

--- src/drf1/views.py
+++ src/drf1/views.py
@@ -1,6 +1,4 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from drf1.models import Product
 from drf1.serializers import ProductSerializers
@@ -9,35 +7,11 @@ from drf1.serializers import ProductSerializers
 # Create your views here.
 
 
-class APIProducts(APIView):
-    def get(self, request):
-        products = Product.objects.all()
-        serializers = ProductSerializers(products, many=True)
-        return Response(serializers.data)
-
-    def post(self, request):
-        serializers = ProductSerializers(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+class APIProducts(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
 
 
-class APIProductDetail(APIView):
-    def get(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        serializer = ProductSerializers(product)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        serializer = ProductSerializers(product, request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class APIProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
